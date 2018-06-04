470d172a486ece1e6f035c4b5e7e56e394ac9df2
Ryouji Matsuzaki <amoroso048@gmail.com>
2018-06-04 15:32:23 +0900

    6/4 クラスの継承　課題

---
+++ CampChallenge_java/013.クラス/クラスの継承/Human.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+
+   class Human{
+    
+        public String name ="";
+        public int age =0;
+
+        public void setHuman(String n,int a){
+        this.name=n;
+        this.age=a;
+        }
+        public void greet(){
+        System.out.println(name);
+        System.out.println(age);
+        }
+    }
+
+
+ 
