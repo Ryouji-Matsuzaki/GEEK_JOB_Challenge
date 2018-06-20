c37b8434c4236f3df4b9d9b7db884f699c354bda
Ryouji Matsuzaki <amoroso048@gmail.com>
2018-06-20 17:16:35 +0900

    6/20 バイト数と文字の取得　課題

---
+++ CampChallenge_java/015.標準クラス/bytetext.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text;
+
+/**
+ *
+ * @author Dell
+ */
+public class bytetext {
+    public static void main(String[] args){
+        String name="松崎亮士";
+        
+        System.out.println(name.getBytes().length);
+        System.out.println(name.length());
+    }
+    
+}
