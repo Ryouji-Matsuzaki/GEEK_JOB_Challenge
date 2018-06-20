b1905c39f9ef0fa7f66351bc3d24c44535751205
Ryouji Matsuzaki <amoroso048@gmail.com>
2018-06-20 17:17:04 +0900

    6/20 文字数の取得　課題

---
+++ CampChallenge_java/015.標準クラス/gettext.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text;
+
+/**
+ *
+ * @author Dell
+ */
+public class gettext {
+    public static void main(String[] args){
+        
+        String mail="amoroso048@gmail.com";
+        
+        String domain=mail.substring(10);
+        
+        System.out.print(domain.length());
+    }
+    
+}
