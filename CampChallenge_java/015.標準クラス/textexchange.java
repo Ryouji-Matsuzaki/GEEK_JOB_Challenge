2ad70fedc8f8bd89d6f7f32231b66324eec24526
Ryouji Matsuzaki <amoroso048@gmail.com>
2018-06-20 17:17:32 +0900

    6/20 文字の入れ替え　課題

---
+++ CampChallenge_java/015.標準クラス/textexchange.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text;
+
+/**
+ *
+ * @author Dell
+ */
+public class textexchange {
+    public static void main(String[] args){
+        
+        String s="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+            s=s.replace("U","う");
+            s=s.replace("I","い");
+            
+        System.out.println(s);
+            
+    }
+    
+}
